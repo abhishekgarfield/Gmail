e0cc557fae8f3fb6c89263b4c7625c9d4f20a8f4
abhishekgarfield <abhishek2759@gmail.com>
2022-10-14 12:09:21 +0530

    form error handling done

--- src/Pages/Authscreen.js
+++ src/Pages/Authscreen.js
@@ -1,11 +1,37 @@
+import { Error } from "@material-ui/icons";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 
 const Authscreen = () => {
-  const [islogin, Setislogin] = useState(true);
+  const [islogin, setislogin] = useState(true);
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
-  console.log(user);
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+    console.log(user);
+  };
+  const handleSubmit = () => {
+    if (
+      user.email.length < 1 ||
+      user.name.length < 1 ||
+      user.password.length < 1 ||
+      user.profile_pic.length < 1
+    ) {
+      return setError(`Fields should not be empty`);
+    } else {
+      if (
+        user.email.match(
+          "(^([a-z-A-Z0-9]+)([@]{1})([A-Za-z]*)([.]{1})([a-zA-Z]{2,4}))"
+        ) == null
+      ) {
+        setError("Email should be of type abhishek@gmail.com");
+      } else if (!islogin && user.password != user.confirmpassword) {
+        setError("Passwords don't match");
+      } else {
+        setError(null);
+      }
+    }
+  };
   return (
     <div className="authscreen-main-container">
       <div className="mid-container">
@@ -16,23 +42,50 @@ const Authscreen = () => {
         <div className="authscreen-info">
           <span>to continue to Gmail</span>
         </div>
-        <div className="authscreen-form">
-          {!islogin && <input type="text" placeholder="Name" name="name" />}
-          <input type="text" placeholder="Email" name="email" />
-          <input type="password" placeholder="Password" name="password" />
+        <div className="authscreen-form"  style={error?{marginBottom:0}:{marginBottom:20}}>
+          {!islogin && (
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={user?.name}
+              onChange={(e) => handleChange(e)}
+            />
+          )}
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={user?.email}
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={user?.password}
+            onChange={(e) => handleChange(e)}
+          />
           {!islogin && (
             <input
               type="password"
               placeholder="Confirm password"
               name="confirmpassword"
+              value={user?.confirmpassword}
+              onChange={(e) => handleChange(e)}
             />
           )}
-
           {!islogin && (
-            <input type="url" placeholder="Profile pic" name="profile_pic" />
+            <input
+              type="url"
+              placeholder="Profile pic"
+              name="profile_pic"
+              value={user?.profile_pic}
+              onChange={(e) => handleChange(e)}
+            />
           )}
         </div>
-        {error && <p>{error}</p>}
+        {error && <p className="error-show"><Error style={{fontSize:18,marginRight:2}}/>{error}</p>}
         <div className="authscreen-footer">
           <div className="aithscreen-options">
             <div className="authscreen-question">
@@ -41,13 +94,23 @@ const Authscreen = () => {
                   ? "Don't have an account?"
                   : "Already having an account?"}
               </span>
-              <span className="qurstion-button">
+              <span
+                className="qurstion-button"
+                onClick={() => {
+                  setislogin(!islogin);
+                  setError(null);
+                }}
+              >
                 {islogin ? "Create account" : "Sign in"}
               </span>
             </div>
           </div>
           <div className="authscreen-submit">
-            <input type="button" value={islogin ? "Signin" : "Sign up"} />
+            <input
+              type="button"
+              value={islogin ? "Signin" : "Sign up"}
+              onClick={handleSubmit}
+            />
           </div>
         </div>
       </div>
